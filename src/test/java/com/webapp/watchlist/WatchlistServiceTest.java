a4b8ea0256738b19f7a4077faca0ead059f78c2f
Mklfcqp <tomas.senti@seznam.cz>
2024-01-20 13:40:03 +0100

    update

---
+++ src/test/java/com/webapp/watchlist/WatchlistServiceTest.java
@@ -0,0 +1,13 @@
+package com.webapp.watchlist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WatchlistServiceTest {
+
+    @Test
+    void addCompanyToWatchlist() {
+        assertEquals(2,1);
+    }
+}
