ba44c36eeefcfb1aebba3e8bfa360a7020716a0c
Tomas Pitron <91890639+Mklfcqp@users.noreply.github.com>
2024-02-08 14:27:38 +0100

    Update WatchlistAddRequest.java

--- src/main/java/com/webapp/watchlist/WatchlistAddRequest.java
+++ src/main/java/com/webapp/watchlist/WatchlistAddRequest.java
@@ -25,6 +25,19 @@ public class WatchlistAddRequest {
     private BigDecimal dividendYield;
     private String overValuedUnderValued;
 
+
+    public getDividendY() {
+        BigDecimal value = dividendQ.multiply(4);
+        dividendY = value.setScale(2, RoundingMode.HALF_UP);
+        return dividendY;
+    }
+
+    public getDividendYield() {
+        BigDecimal value = (dividendQ.divide(price)).multiply(100);
+        dividendYield = value.setScale(2, RoundingMode.HALF_UP);
+        return dividendYield;
+    }
+
     public String getOverValuedUnderValued() {
         BigDecimal value = (((price.divide(dcf)).multiply(BigDecimal.valueOf(100))).subtract(BigDecimal.valueOf(100)));
 
